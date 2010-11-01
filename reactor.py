ec25ffc51320066915461bbaa18ebac2e83fce6b
Christian Wörner <christianworner@gmail.com>
2010-11-01 15:49:52 +0100

    add exception handling for reactor: receive data

--- reactor.py
+++ reactor.py
@@ -1,33 +1,39 @@
 import couchdb
 import json
-from twisted.internet.protocol import DatagramProtocol
+from twisted.application import internet, service
+from twisted.internet.protocol import  DatagramProtocol
 from twisted.internet import reactor
 
 class Charun(DatagramProtocol):
     def __init__(self,couch_connect):
         self.couch_connect = couch_connect
     def datagramReceived(self, data, (host, port)):
-        print "received %r , from %s:%d" % (data, host, port)
-        obj = json.loads(data)
-        print "received %r ,transformed it to %r , from %s:%d" % (data, obj, host, port)
-        self.couch_connect.store(obj)
-        #self.transport.write(data, (host, port))
+        log.msg("received %r , from %s" % (data, host), logLevel=logging.DEBUG)
+        try:
+            obj = json.loads(data)
+            obj["_id"] = repr(time.time())
+            origin = (host,port)
+            self.couch_connect.store(obj, origin)
+        except TypeError as error: 
+            log.err(" - ".join(repr(error).split("\n")))
 
 class CouchConnect():
-    def __init__(self, url = "http://localhost:5984"):
+    from twisted.python import log
+    
+    def __init__(self, url , db_name):
         self.couch = couchdb.Server(url)
         db_name = "charun_usage"
         if db_name not in self.couch.resource.get_json("_all_dbs")[2]: 
             self.db = self.couch.create(db_name)
         else:
             self.db = self.couch[db_name]
-    def store(self, _dict):
+    def store(self, _dict, origin):
         if type(_dict).__name__ == "dict":
             self.db.save(_dict)
         else: 
-            raise TypeError("object to store must be of type 'dict'\nwas: %s" % type(_dict).__name__)
-            
-        
+            msg = "object to store must be of type 'dict' but was: '%s'" % (type(_dict).__name__,)
+            log.msg("** TypeError: from: %s - msg: %s" % (origin[0], msg), logLevel=logging.ERROR) 
+
 
 if __name__ == "__main__":
     cc = CouchConnect()
