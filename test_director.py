8b306345544443451b34641e3af4c2968f4ed2da
Christian Wörner <christianworner@gmail.com>
2010-11-10 01:03:15 +0100

    add option to run only unit/integration tests
    * call the 'test_director' with unit/integration argument, such as:
    python test_director.py unit

--- test_director.py
+++ test_director.py
@@ -1,9 +1,23 @@
+"""this script orchestrates the tests contained in the tests folder"""
+import sys
 import unittest
-import tests.unittest_charun as ut_char
-
-
-
+import tests.integration_test_charun as i_t_charun
+import tests.unit_test_charun as u_t_charun
 
 if __name__ == "__main__":
-    
-    unittest.TextTestRunner(verbosity=2).run(ut_char.suite())
+    uniT, integratioN = False, False
+    print "script called with %r arguments" % sys.argv
+    la = len(sys.argv) 
+    if  la == 1:
+        uniT,integratioN = True,True
+    elif la > 1:
+        what = sys.argv[1]
+        if what == "unit": uniT = True
+        elif what == "integration": integratioN = True
+        else: print "Usage:\ncall the script with 'unit' XOR 'integration' arguments to run only the specified test-suite.\ncalling it without arguments will result in running all tests."
+    if uniT:
+        print "starting unit tests...\n---------------------------"
+        unittest.TextTestRunner(verbosity=2).run(u_t_charun.suite())
+    if integratioN:
+        print "starting integration tests...\n-----------------------------"
+        unittest.TextTestRunner(verbosity=2).run(i_t_charun.suite())
