3dd587622f9d2a825213fe9e6bde5048aa15b042
Christian Wörner <christianworner@gmail.com>
2010-12-09 19:50:15 +0100

    comply with pep8

--- tests/unit_test_couchdb_connect.py
+++ tests/unit_test_couchdb_connect.py
@@ -11,6 +11,7 @@ import client
 import charun_tac
 from couchdb_connect import CouchDBConnect
 
+
 class testCouchDBConnect(unittest.TestCase):
     """a unit-test class for the CouchDBConnect class"""
     def setUp(self):
@@ -29,22 +30,25 @@ class testCouchDBConnect(unittest.TestCase):
         self.assertTrue(cc)
 
     def test_12_failing_create_instance_without_couchdb_running(self):
-        """couchdb_connect: test that no couchdbconnect instance is created without couch listening"""
-        self.assertRaises(IOError, CouchDBConnect,"http://localhost:8899","should have raised an error but didn't")
-    
+        """couchdb_connect: assert that no couchdbconnect instance
+        is created without couch listening"""
+        self.assertRaises(IOError, CouchDBConnect, "http://localhost:8899",
+                          "should have raised an error but didn't")
+
     def test_20_store_dict(self):
         """an incoming dict should get stored"""
         cc = CouchDBConnect()
         cc.db = Mock()
-        cc.store({12:13},("localhost",30000))
+        cc.store({12: 13}, ("localhost", 30000))
         self.assertTrue(cc.db.save.called)
-         
+
     def test_30_do_not_store_non_dict(self):
-        """an incoming non-dict should not get stored and an error message written"""
+        """an incoming non-dict should not get stored and
+        an error message written"""
         cc = CouchDBConnect()
         cc.db = Mock()
         cc.log = Mock()
-        cc.store("am I a dict?",("localhost",30000))
+        cc.store("am I a dict?", ("localhost", 30000))
         self.assertFalse(cc.db.save.called)
         self.assertFalse(cc.log.msg.called)
         self.assertTrue(cc.log.err.called)
@@ -59,7 +63,3 @@ def suite():
 
 if __name__ == '__main__':
     unittest.TextTestRunner(verbosity=2).run(suite())
-
-
-
-
